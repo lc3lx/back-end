e0ee84603c21f2afe4485ed989cfbab9ab49f1f2
lc3lx <140909706+lc3lx@users.noreply.github.com>
2024-06-08 19:57:57 +0300

    order.js

--- router/order.js
+++ router/order.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const {getthedata,resrverdata,updatedata,deletedata} = require("../controller/ordercontroller")
 
 
-router.post("/",resrverdata)
-router.get("/",getthedata)
+router.post("/sendorder",resrverdata)
+router.get("/getorder",getthedata)
 router.put("/:id",updatedata)
 router.delete("/:id",deletedata)
 module.exports = router
