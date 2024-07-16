5acc4c0d085f1d6ec5cfc74d6c775d9a5808caf9
lc3lx <140909706+lc3lx@users.noreply.github.com>
2024-07-16 15:22:06 +0300

    Add files via upload

---
+++ model/Recyclbin.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+
+
+
+const recyclbinschema = new mongoose.Schema(
+    {
+    customarname :{
+        type:String ,
+        minlength : 3,
+        maxlength : 200,
+        required: true
+    },
+    customarphone :{
+        type : String ,
+        minlength:9,
+        maxlength:14,
+        required: true
+    },
+    locationCar : {
+        type: String,
+        required : true ,
+        
+    },
+    timeofserves :{
+        type:String ,
+        required: true
+    }
+
+
+
+},{timestamps:true})
+
+
+const Recyclbin = mongoose.model("Recyclbin",recyclbinschema)
+module.exports = Recyclbin
