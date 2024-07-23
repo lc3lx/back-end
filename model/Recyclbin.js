e7c1f8c08975c3e89d8ad95d567156187064a9f7
lc3lx <140909706+lc3lx@users.noreply.github.com>
2024-07-23 13:08:57 +0300

    Recyclbin.js

--- model/Recyclbin.js
+++ model/Recyclbin.js
@@ -8,25 +8,35 @@ const recyclbinschema = new mongoose.Schema(
         type:String ,
         minlength : 3,
         maxlength : 200,
-        required: true
+       
     },
     customarphone :{
         type : String ,
         minlength:9,
         maxlength:14,
-        required: true
+       
     },
     locationCar : {
         type: String,
-        required : true ,
+       
         
     },
-    
+    timeofserves :{
+        type:String ,
+        
+
+    },
+    local:{
+        type:String,
+        
+    }
+   }
 
 
 
-},{timestamps:true})
+,{timestamps:true})
 
 
 const Recyclbin = mongoose.model("Recyclbin",recyclbinschema)
 module.exports = Recyclbin
+
