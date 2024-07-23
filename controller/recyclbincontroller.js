a952c72210e3a9cbd91e7dbcd5b7ac7024e40108
lc3lx <140909706+lc3lx@users.noreply.github.com>
2024-07-23 13:07:34 +0300

    recyclbincontroller.js

--- controller/recyclbincontroller.js
+++ controller/recyclbincontroller.js
@@ -16,6 +16,15 @@ const resrverdata = asynchandler(async(req,res)=>{
     const ruesult = await recyclbin.save()
     res.status(201).json(ruesult)
 })
+const deleteformrecyclbin = asynchandler(async(req,res)=>{
+    const recyclbin = await Recyclbin.findById(req.params.id)
+    if(!recyclbin ){
+        return res.status(404).json({message :"the order id not found"})
+    }
+    await Recyclbin.findByIdAndDelete(req.params.id)
+    res.status(200).json({message :" the order hse been deleted sccussfully"})
+})
 module.exports ={
     getthedata,
-    resrverdata}
+    resrverdata,
+deleteformrecyclbin}
