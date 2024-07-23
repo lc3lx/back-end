e529d2b6b79301e5d13c89f4a3dbd908eed1eeb5
lc3lx <140909706+lc3lx@users.noreply.github.com>
2024-07-23 13:08:12 +0300

    recyclbin.js

--- router/recyclbin.js
+++ router/recyclbin.js
@@ -3,4 +3,5 @@ const router = express.Router()
 const {getthedata,resrverdata} = require("../controller/recyclbincontroller")
 router.post("/postdata",resrverdata)
 router.get("/getdata",getthedata)
+router.delete("/deleteorder/:id")
 module.exports = router
